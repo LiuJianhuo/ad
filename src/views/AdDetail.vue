ff5a30f9fd69baa5b77523ba120273376163aad6
ljh <2513604118@qq.com>
2019-11-30 15:19:02 +0800

    init

---
+++ src/views/AdDetail.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="page">
+      <mt-header title="任务详情"></mt-header>
+        <div class="avatar circle">
+          <img :src="logo"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import logo from '@/assets/logo.png'
+
+export default {
+  name: 'AdDetail',
+  data: function () {
+    return {
+      logo
+    }
+  }
+}
+</script>
+
+<style  scoped>
+.page {
+  width: 100%;
+}
+
+.avatar {
+  display: inline-block;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  text-align: center;
+  overflow: hidden;
+  color: #fff;
+  background: #C0C4CC;
+  width: 50px;
+  height: 50px;
+  line-height: 50px;
+}
+
+.avatar img {
+  display: block;
+  vertical-align: middle;
+  height: 100%;
+}
+
+</style>
